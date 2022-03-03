9b3925031f4b15e7662f57c5df1e8861a4a74746
Babywise <islamdoesart@gmail.com>
2022-03-02 20:54:27 -0500

    Created PacketQuitGame

---
+++ BlackJackServer/PacketQuitGame.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Packet.h"
+
+class PacketQuitGame : public Packet {
+
+	struct Body {
+		bool status;
+	} Body;
+
+public:
+	// creating
+	PacketQuitGame() : Packet() { 
+		Head.Source = 0, Head.Destination = 0, Head.Bytes = sizeof(Body) + emptyPacketSize, Head.Fin = 0, Head.Ack = 0, Head.pType = PacketType::packetQuitGame;
+		Body.status = false;
+	};
+
+	// receiving
+	PacketQuitGame(char* src) {
+		std::memcpy(&this->Head, src, sizeof(Head));
+		std::memcpy(&this->Body, src + sizeof(Head), sizeof(Body));
+
+		this->pSerialBuff = nullptr;
+		this->buffer = nullptr;
+
+	}
+
+	void setStatus(int status) {
+		this->Body.status = status;
+	}
+
+	int setStatus() {
+		return this->Body.status;
+	}
+
+	char* serialize() {
+		unsigned int TotalSize = emptyPacketSize + this->Head.Bytes;
+
+		this->pSerialBuff = new char[TotalSize] { NULL };
+
+		std::memcpy(this->pSerialBuff, &this->Head, sizeof(Head));
+		std::memcpy(this->pSerialBuff + sizeof(Head), &this->Body, this->Head.Bytes);
+
+		return this->pSerialBuff;
+
+	}
+};
