260b77ae30f7aae1ca8a824b1ec648ccdcb74189
Babywise <islamdoesart@gmail.com>
2022-03-02 20:51:42 -0500

    Created PacketAddFunds

---
+++ BlackJackServer/PacketAddFunds.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Packet.h"
+
+class PacketAddFunds : public Packet {
+
+	struct Body {
+		Account* acc;
+		long cardNumber;
+		int amountToAdd;
+	} Body;
+
+public:
+	// creating
+	PacketAddFunds() : Packet() { 
+		Head.Source = 0, Head.Destination = 0, Head.Bytes = sizeof(Body) + emptyPacketSize, Head.Fin = 0, Head.Ack = 0, Head.pType = PacketType::packetAddFunds,
+		Body.acc = NULL, Body.cardNumber = 0, Body.amountToAdd = 0;
+	};
+
+	// receiving
+	PacketAddFunds(char* src) {
+		std::memcpy(&this->Head, src, sizeof(Head));
+		std::memcpy(&this->Body, src + sizeof(Head), sizeof(Body));
+
+		this->pSerialBuff = nullptr;
+		this->buffer = nullptr;
+
+	}
+
+	void setAccount(Account* acc) {
+		this->Body.acc = acc;
+	}
+
+	Account* getAccount() {
+		return this->Body.acc;
+	}
+
+	void setCardNumber(long cardNumber) {
+		this->Body.cardNumber = cardNumber;
+	}
+
+	long getCardNumber() {
+		return this->Body.cardNumber;
+	}
+
+	void setAmountToAdd(long amountToAdd) {
+		this->Body.amountToAdd = amountToAdd;
+	}
+
+	long getAmountToAdd() {
+		return this->Body.amountToAdd;
+	}
+
+	char* serialize() {
+		unsigned int TotalSize = emptyPacketSize + this->Head.Bytes;
+
+		this->pSerialBuff = new char[TotalSize] { NULL };
+
+		std::memcpy(this->pSerialBuff, &this->Head, sizeof(Head));
+		std::memcpy(this->pSerialBuff + sizeof(Head), &this->Body, this->Head.Bytes);
+
+		return this->pSerialBuff;
+
+	}
+};
