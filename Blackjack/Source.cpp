ed68a5bf0d36b5b0914b11fa7f17d37e81124359
techee12 <70961125+techee12@users.noreply.github.com>
2022-03-11 12:15:54 -0500

    Account object added to this branch by mistake. No conflicts, 8  tests added (8/8) Passing

--- Blackjack/Source.cpp
+++ Blackjack/Source.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+#include <fstream>
+using namespace std;
 
 int main()
 {
 
    
     printf("Hello World");
+	fstream f;
+	f.open(("./Users/Test"), ios::out);
+	f.close();
 
     return 0;
 }
