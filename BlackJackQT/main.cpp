d45fc70717f3563e115ad5068f31a1b6cd3bd602
Nicholas Prince <nickvprince@gmail.com>
2022-03-25 16:43:40 -0400

    Added widget container to main ui form file

--- BlackJackQT/main.cpp
+++ BlackJackQT/main.cpp
@@ -1,10 +1,17 @@
 #include "BlackJackQT.h"
 #include <QtWidgets/QApplication>
 
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     BlackJackQT w;
     w.show();
+
+	cout << "Hello World!" << endl;
+
     return a.exec();
 }
