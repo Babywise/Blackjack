9222f55cfb31df90546258083a07abb4102fe5c9
techee12 <70961125+techee12@users.noreply.github.com>
2022-03-25 18:08:17 -0400

    packet modifications

--- Blackjack/PacketLogin.h
+++ Blackjack/PacketLogin.h
@@ -1,1 +1,52 @@
 #pragma once
+#include "Packet.h"
+
+class PacketLogin : public Packet {
+
+	struct Body {
+		string username;
+		string password;
+	} Body;
+
+public:
+	// creating
+	PacketLogin() : Packet() { Head.Source = 0, Head.Destination = 0, Head.Bytes = sizeof(Body) + emptyPacketSize, Head.Fin = 0, Head.Ack = 0, Head.pType = PacketType::packetLogin; };
+
+	// receiving
+	PacketLogin(char* src) {
+		std::memcpy(&this->Head, src, sizeof(Head));
+		std::memcpy(&this->Body, src + sizeof(Head), sizeof(Body));
+
+		this->pSerialBuff = nullptr;
+		this->buffer = nullptr;
+
+	}
+
+	void PacketLoginSetUsername(string username) {
+		this->Body.username = username;
+	}
+
+	string PacketLoginGetUsername() {
+		return this->Body.username;
+	}
+
+	void PacketLoginSetPassword(string password) {
+		this->Body.password = password;
+	}
+
+	string PacketLoginGetPassword() {
+		return this->Body.password;
+	}
+
+	char* serialize() {
+		unsigned int TotalSize = emptyPacketSize + this->Head.Bytes;
+
+		this->pSerialBuff = new char[TotalSize] { NULL };
+
+		std::memcpy(this->pSerialBuff, &this->Head, sizeof(Head));
+		std::memcpy(this->pSerialBuff + sizeof(Head), &this->Body, this->Head.Bytes);
+
+		return this->pSerialBuff;
+
+	}
+};
