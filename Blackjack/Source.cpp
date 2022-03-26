dd50dd1dfc5ad581c566481e311faac94bd556cb
techee12 <70961125+techee12@users.noreply.github.com>
2022-03-26 19:33:55 -0400

    Error Log functionality implemented Log folder must exist for this to work

--- Blackjack/Source.cpp
+++ Blackjack/Source.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fstream>
+#include "Logging.h"
 using namespace std;
 
 int main()
@@ -7,6 +8,10 @@ int main()
 
    
     printf("Hello World");
-
+    ErrorLog("Error", "Severe");
+    string errors[5];
+    errors[0] = "Client Recieved xxx";
+    errors[1] = "Thread 2001";
+    DataLog("Client send Ack");
     return 0;
 }
